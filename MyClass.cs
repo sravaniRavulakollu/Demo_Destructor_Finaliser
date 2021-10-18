19c40ef3b911ad7e8a250817510c4939d01db17b
sravaniRavulakollu <87975506+sravaniRavulakollu@users.noreply.github.com>
2021-10-18 15:40:04 +0530

    Add files via upload

---
+++ MyClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Demo_Destructer_Finalizer
+{
+    class MyClass
+    {
+        public void Display_Msg()
+        {
+            Console.WriteLine("Inside Display Method");
+        }
+        public MyClass()
+        {
+            Console.WriteLine("Default constructer...");
+        }
+        public MyClass(string msg)
+        {
+            Console.WriteLine("Parametrised constructor with a message {0}", msg);
+        }
+        //~MyClass()
+        //{
+        // Console.WriteLine("Freeing up Resources...");
+        //}
+
+        public void Dipose()
+        {
+            Console.WriteLine("Freeing up resources");
+            GC.SuppressFinalize(this);
+
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Demo_Destructer_Finalizer;
+
+
+
+namespace Demo_Destructer_Finalizer
+{
+    class Program : MyClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Demo Finalizer!");
+            MyClass obj1 = new MyClass();
+            obj1.Display_Msg();
+
+
+
+            //MyClass obj2 = new MyClass("Secret msg");
+            //obj2.Display_Msg();
+
+
+
+            obj1.Dipose();
+
+
+
+        }
+    }
+}
